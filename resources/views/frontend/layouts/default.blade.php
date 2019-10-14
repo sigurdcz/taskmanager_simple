68f219d6b42bd9683fdf8bb97deed1d59cc05326
Sigurd Gnezdilov <sigurd.gnezdilov@gmail.com>
2019-10-14 16:45:37 +0200

    dev - add boostrap
    pridani stylu od boostrap

--- resources/views/frontend/layouts/default.blade.php
+++ resources/views/frontend/layouts/default.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   <title>Manager</title>
   <meta charset="utf-8">
@@ -7,9 +7,21 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link href="{{asset('css/offcanvas.css')}}" rel="stylesheet">
 </head>
 <body>
 
+
+<div class="nav-scroller bg-white box-shadow">
+  <nav class="nav nav-underline">
+    <a class="nav-link active" href="#">Homepage</a>
+    <a class="nav-link" href="{{route('tasks.index')}}">
+      Tasks
+      <span class="badge badge-pill bg-light align-text-bottom">27</span>
+    </a>
+
+  </nav>
+</div>
 <div class="container">
     @yield('content')
 </div>
