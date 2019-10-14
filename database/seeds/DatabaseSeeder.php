a6ca9b0cdb45e4838a5dfc97745981fbb65ea3dd
Sigurd Gnezdilov <sigurd.gnezdilov@gmail.com>
2019-10-14 15:52:32 +0200

    dev - database - base seeder
    Nastaveny zakladni seedy pro tasky a komentare

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Model\Comment;
+use App\Model\Status;
+use App\Model\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // Vytvoření seedu pro STATUS tasku
+        Status::create(['name'=>'success']);
+        Status::create(['name'=>'created']);
+
+        /**
+         * vytvoreni zaznamu deseti tasku s nahodnym poctem komentaru
+         *  celkovy pocet komentaru je roven deseti
+         *  Vysledek je - komentar je nahodne rozdelen k tasku
+         */
+        factory(Task::class, 10)->create();
+        factory(Comment::class, 10)->create();
+
     }
 }
