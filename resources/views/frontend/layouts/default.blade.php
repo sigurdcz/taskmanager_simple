fd5b264bf3d9dec5d243d2fe6e23ce0fce111279
Sigurd Gnezdilov <Sigurd.Gnezdilov@leksys.cz>
2019-10-15 17:18:29 +0200

    fix - favicon path

--- resources/views/frontend/layouts/default.blade.php
+++ resources/views/frontend/layouts/default.blade.php
@@ -4,7 +4,7 @@
   <title>Manager</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="images/png" sizes="32x32" href="{{asset('/images/png')}}">
+  <link rel="icon" type="images/png" sizes="32x32" href="{{asset('/images/system/favicon-32x32.png')}}">
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
